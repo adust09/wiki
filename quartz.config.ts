9cf3b9d3c2c9a258563b0f2918a2c89160ac3be9
adust09 <shoukitsuda@icloud.com>
2024-09-29 20:49:59 +0900

    return

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "wiki-92c.pages.dev/quartz",
+    baseUrl: "wiki-92c.pages.dev",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
