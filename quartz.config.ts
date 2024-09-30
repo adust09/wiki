cbcf7fc34a60beaf0d43653924619567297b3f1d
adust09 <shoukitsuda@icloud.com>
2024-09-30 21:43:04 +0900

    fix path

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "adust09.github.io/quartz",
+    baseUrl: "wiki-92c.pages.dev",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
